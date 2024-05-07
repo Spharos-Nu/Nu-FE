72fb80f14a50a29fbd86dd56b0cfce54322363e1
yunii23 <yunii23@naver.com>
2024-05-07 14:07:19 +0900

    Design: 인트로 화면 퍼블리싱 (#6)

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,10 @@
+// import Intro from '@/containers/main/Intro'
+
+export default function Home() {
+  return (
+    <main className="w-full">
+      {/* <Intro /> */}
+      <h1>Home</h1>
+    </main>
+  )
+}
