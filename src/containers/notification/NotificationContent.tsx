86fe70aa3a40d8d4b760841d0025a883c54f1731
yunii23 <munii1023@gmail.com>
2024-05-28 15:31:29 +0900

    Design: 알림 화면 퍼블리싱

---
+++ src/containers/notification/NotificationContent.tsx
@@ -0,0 +1,28 @@
+import { PiTrashLight } from 'react-icons/pi'
+import { NotificationType } from '@/types/NotificationType'
+
+export default function NotificationContent({
+  data,
+}: {
+  data: NotificationType
+}) {
+  // const deleteNotification = async () => {
+  //   try {
+  //     // delete notification
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
+
+  return (
+    <div className="bg-[#f7f7f7] my-[15px] px-[20px] py-[15px] rounded-3xl h-[80px]">
+      <p className="text-[17px] leading-[20px]">{data.content}</p>
+      <div className="flex justify-between">
+        <p className="text-[14px] leading-[40px]">{data.created_at}</p>
+        <div className="content-center">
+          <PiTrashLight className="w-[20px] h-[20px]" />
+        </div>
+      </div>
+    </div>
+  )
+}
