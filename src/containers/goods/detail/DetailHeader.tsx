df1f4c402bb9775d4174cfeedf731e8340e759a1
yunii23 <munii1023@gmail.com>
2024-06-11 15:37:57 +0900

    Design: 상품 상세 퍼블리싱 중

---
+++ src/containers/goods/detail/DetailHeader.tsx
@@ -0,0 +1,9 @@
+import BackBtn from '@/components/Btn/BackBtn'
+
+export default function DetailHeader() {
+  return (
+    <header className="relative h-[60px] text-[#2B74B9]">
+      <BackBtn />
+    </header>
+  )
+}
