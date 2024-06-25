f0853b33ac099669ddb8e5fde6d7b0eb9cf9a17e
wnsvy7203 <wnsvy7203@gmail.com>
2024-06-25 15:00:38 +0900

    Fix: middleware

--- src/app/mypage/update-password/page.tsx
+++ src/app/mypage/update-password/page.tsx
@@ -1,5 +1,21 @@
 import ChangePasswordForm from '@/containers/mypage/update-password/ChangePasswordForm'
+// import { updatePassword } from '@/utils/authApiActions'
 
-export default function UpdatePassword() {
+export default async function UpdatePassword() {
+  // const updatePass = async (updateForm: FormData) => {
+  //   'use server'
+
+  //   const payload = {
+  //     currentPassword: updateForm.get('currentPassword')!,
+  //     newPassword: updateForm.get('newPassword'),
+  //   }
+
+  //   const res = await updatePassword(
+  //     payload.currentPassword,
+  //     payload.newPassword!,
+  //   )
+  //   console.log(res)
+  // }
+
   return <ChangePasswordForm />
 }
