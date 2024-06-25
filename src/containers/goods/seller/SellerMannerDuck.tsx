ef6e49282dff9840ce22fb3e0f1ff4ea05286ae1
yunii23 <munii1023@gmail.com>
2024-06-25 12:15:56 +0900

    Feat: 판매자 정보 조회 데이터 패칭

--- src/containers/goods/seller/SellerMannerDuck.tsx
+++ src/containers/goods/seller/SellerMannerDuck.tsx
@@ -1,19 +1,49 @@
-import Image from 'next/image'
-import img from '@/dummydata/profile.png'
+import MannerDuck1 from '@/public/svgs/duck/mannerDuckLevel1.svg'
+import MannerDuck2 from '@/public/svgs/duck/mannerDuckLevel2.svg'
+import MannerDuck3 from '@/public/svgs/duck/mannerDuckLevel3.svg'
+import MannerDuck4 from '@/public/svgs/duck/mannerDuckLevel4.svg'
+import MannerDuck5 from '@/public/svgs/duck/mannerDuckLevel5.svg'
+import { SellerMannerType } from '@/types/goodsType'
 
-export default function SellerMannerDuck() {
+export default function SellerMannerDuck({
+  sellerMannerDuck,
+  nickname,
+}: {
+  sellerMannerDuck: SellerMannerType
+  nickname: string
+}) {
+  let mannerDuckLabel = ''
+  switch (sellerMannerDuck.level) {
+    case 1:
+      mannerDuckLabel = '해골 오리'
+      break
+    case 2:
+      mannerDuckLabel = '우는 오리'
+      break
+    case 3:
+      mannerDuckLabel = '오리'
+      break
+    case 4:
+      mannerDuckLabel = '선글라스 오리'
+      break
+    case 5:
+      mannerDuckLabel = '빛나는 오리'
+      break
+    default:
+      mannerDuckLabel = '오리'
+  }
+
   return (
     <div className="mt-[20px] mx-[20px] px-[25px] pt-[15px] pb-[25px] bg-[#f7f7f7] rounded-2xl">
       <p className="text-[15px] text-[#818181]">매너덕</p>
-      <Image
-        src={img}
-        alt="판매자 매너덕"
-        width={100}
-        height={100}
-        className="m-auto rounded-full"
-      />
+      {sellerMannerDuck.level === 1 && <MannerDuck1 />}
+      {sellerMannerDuck.level === 2 && <MannerDuck2 />}
+      {sellerMannerDuck.level === 3 && <MannerDuck3 />}
+      {sellerMannerDuck.level === 4 && <MannerDuck4 />}
+      {sellerMannerDuck.level === 5 && <MannerDuck5 />}
       <p className="text-center mt-[10px] text-black">
-        <span className="font-semibold">닉네임</span> 님의 매너덕은 오리입니다.
+        <span className="font-semibold">{nickname}</span> 님의 매너덕은{' '}
+        {mannerDuckLabel}입니다.
       </p>
     </div>
   )
