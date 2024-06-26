09d3f9c32b9b434f973fefdb070a1e71bb4a46f5
yunii23 <munii1023@gmail.com>
2024-06-26 13:43:55 +0900

    Refactor: 덕포인트 패칭 위치 변경

--- src/containers/main/UserHeader.tsx
+++ src/containers/main/UserHeader.tsx
@@ -1,27 +1,12 @@
-'use client'
-
-import { useEffect, useState } from 'react'
-import { getDuckPoint } from '@/utils/mainApiActions'
 import UserDuckPoint from './UserDuckPoint'
 import UserProfile from './UserProfile'
 
 export default function UserHeader() {
-  const [duckPoint, setDuckPoint] = useState<number>(0)
-
-  useEffect(() => {
-    const getData = async () => {
-      const DuckPointData = await getDuckPoint()
-
-      setDuckPoint(DuckPointData.result)
-    }
-    getData()
-  }, [])
-
   return (
     <div className="flex justify-between pt-[30px] px-[20px]">
       <UserProfile />
       <div className="content-center">
-        <UserDuckPoint duckPoint={duckPoint} />
+        <UserDuckPoint />
       </div>
     </div>
   )
