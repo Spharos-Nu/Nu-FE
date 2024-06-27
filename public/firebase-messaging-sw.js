006ca2e3ff66b159c7008ea49dd33a33b089df3f
wnsvy7203 <wnsvy7203@gmail.com>
2024-06-27 11:44:15 +0900

    Fix: 파이어베이스

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -23,3 +23,18 @@ firebase.initializeApp({
 
 // Retrieve Firebase Messaging object
 const messaging = firebase.messaging()
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload,
+  )
+  // Customize notification here
+  const notificationTitle = 'Background Message Title'
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png',
+  }
+
+  self.registration.showNotification(notificationTitle, notificationOptions)
+})
--- src/containers/member/login/LoginForm.tsx
+++ src/containers/member/login/LoginForm.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { initializeApp } from 'firebase/app'
-import { getMessaging, getToken } from 'firebase/messaging'
+import 'firebase/messaging'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { signIn } from 'next-auth/react'
@@ -10,19 +9,9 @@ import { FaCheckSquare } from 'react-icons/fa'
 import { TiDelete } from 'react-icons/ti'
 import { useBasicAlertStore } from '@/components/Modal/store'
 import { montserrat } from '@/styles/fonts'
+import { getToken } from '@/utils/firebase'
 import { saveId, getId, saveCheckbox, getCheckbox } from '@/utils/localStorage'
 import { saveDeviceToken } from '@/utils/notificationApiActions'
-
-const firebaseApp = initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-})
-const messaging = getMessaging(firebaseApp)
 
 export default function LoginForm() {
   const router = useRouter()
@@ -79,14 +68,8 @@ export default function LoginForm() {
     })
 
     if (res?.status === 200) {
-      getToken(messaging, {
-        vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
-      }).then(async (currentToken) => {
-        if (currentToken) {
-          // console.log(currentToken)
-          await saveDeviceToken(currentToken)
-        }
-      })
+      const token = await getToken()
+      await saveDeviceToken(token)
       router.push(params)
     } else {
       showAlert('회원정보가 일치하지 않습니다.')
