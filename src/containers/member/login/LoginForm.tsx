944c69a239fc6dd9cda0590a305ed36dc375ce7b
Doyeon <ansehdus7@gmail.com>
2024-06-27 14:40:55 +0900

    Feat: firebaseApp수정

--- src/containers/member/login/LoginForm.tsx
+++ src/containers/member/login/LoginForm.tsx
@@ -16,9 +16,9 @@ import { saveDeviceToken } from '@/utils/notificationApiActions'
 
 const firebaseApp = initializeApp({
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: 'goodsgoodsduck.firebaseapp.com',
-  projectId: 'goodsgoodsduck',
-  storageBucket: 'goodsgoodsduck.appspot.com',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
