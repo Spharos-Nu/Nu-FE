bea8003d97bf208510ec44ef6c13e42f1c9b1154
yunii23 <munii1023@gmail.com>
2024-06-27 21:54:10 +0900

    Refactor: 입찰 리스트 데이터 패칭 진행중

--- src/components/GoodsSummary/index.tsx
+++ src/components/GoodsSummary/index.tsx
@@ -2,75 +2,73 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { GoHeart, GoHeartFill } from 'react-icons/go'
-import { SummaryData } from '@/types/goodsApiDataType'
-import { getGoodsSummary, getLike } from '@/utils/goodsApiActions'
+import { BiddingType } from '@/types/goodsApiDataType'
 
-export default function GoodsSummary({ goodsCode }: { goodsCode: number }) {
-  const [data, setData] = useState<SummaryData>({
-    thumbnailUrl: '',
-    goodsName: '',
-    minPrice: 0,
-    openedAt: '',
-    closedAt: '',
-    tradingStatus: 0,
-  })
-  const [like, setLike] = useState<boolean>(false)
+export default function GoodsSummary({
+  goodsList,
+}: {
+  goodsList: BiddingType
+}) {
+  // const [data, setData] = useState<BiddingType>({
+  //   goodsCode: '',
+  //   name: '',
+  //   price: 0,
+  //   thumbnail: '',
+  // })
+  // const [like, setLike] = useState<boolean>(false)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await getGoodsSummary(goodsCode)
-      if (res.status === 200) {
-        setData(res.result)
-      }
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await getGoodsSummary(goodsCode)
+  //     if (res.status === 200) {
+  //       setData(res.result)
+  //     }
 
-      const res2 = await getLike(goodsCode)
-      if (res2.status === 200) {
-        if (res2.result) {
-          setLike(true)
-        } else {
-          setLike(false)
-        }
-      }
-    }
+  //     const res2 = await getLike(goodsCode)
+  //     if (res2.status === 200) {
+  //       if (res2.result) {
+  //         setLike(true)
+  //       } else {
+  //         setLike(false)
+  //       }
+  //     }
+  //   }
 
-    fetchData()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  //   fetchData()
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [])
 
   return (
-    <Link
-      href={`/goods-detail/?code=${goodsCode}`}
-      className="w-[90%] h-[90%] m-auto relative"
-    >
+    <Link href={`/goods/goodsCode=${goodsList.goodsCode}`} className="relative">
       <Image
-        src={data.thumbnailUrl}
+        src={goodsList.thumbnail}
         alt="썸네일"
-        className="rounded-t-xl w-full h-full"
+        className="rounded-t-xl w-full h-full aspect-square object-cover"
         width={0}
         height={0}
+        sizes="100vw"
       />
       <div className="rounded-b-xl">
-        <p>{data.goodsName}</p>
+        <p className="truncate">{goodsList.name}</p>
         <div className="grid grid-cols-3 gap-1">
-          <div className="col-span-2 px-3 py-1">
+          {/* <div className="col-span-2 px-3 py-1">
             <span className="rounded-2xl bg-sky-200 text-sky-500 text-center">
-              {data.tradingStatus === 0 ? data.openedAt : data.closedAt}
+              {goodsList.tradingStatus === 0 ? data.openedAt : data.closedAt}
             </span>
-          </div>
+          </div> */}
           <div>
             <span className="rounded-2xl bg-sky-600 text-center">
-              {data.minPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
+              {goodsList.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+              원
             </span>
           </div>
         </div>
       </div>
-      {like ? (
+      {/* {like ? (
         <GoHeartFill className="text-white absolute right-3 top-3" />
       ) : (
         <GoHeart className="text-red-500 absolute right-3 top-3" />
-      )}
+      )} */}
     </Link>
   )
 }
--- src/containers/main/ComingSoon.tsx
+++ src/containers/main/ComingSoon.tsx
@@ -19,7 +19,7 @@ export default function ComingSoon({
             <p>굿즈를 등록해보세요!</p>
           </div>
         )}
-        <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:w-4grid-cols-3 lg:grid-cols-5 xl:grid-cols-6">
+        <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
           {comingSoonData.map((item: SoonAndHitsType) => (
             <SoonAndHitsItem key={item.goodsCode} item={item} sort={sort} />
           ))}
