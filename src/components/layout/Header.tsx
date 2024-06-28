64871256e786dd7d2cdb1c20c2faac6cb0dc7658
wnsvy7203 <wnsvy7203@gmail.com>
2024-06-28 19:36:55 +0900

    Feat: noti

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,6 @@ import { useEffect, useState } from 'react'
 import { IoMdNotificationsOutline, IoMdSearch } from 'react-icons/io'
 import { useHeaderModalState } from '@/components/layout/store'
 import CategorySelection from '@/containers/category/CategorySelection'
-import NotificationModal from '@/containers/main/notification/NotificationModal'
 import SearchModal from '@/containers/main/search/SearchModal'
 import Logo from '@/public/svgs/header/logo.svg'
 import { getNotiCount } from '@/utils/notificationApiActions'
@@ -14,7 +13,7 @@ import { getNotiCount } from '@/utils/notificationApiActions'
 export default function Header() {
   const [num, setNum] = useState<number>(0)
   const pathname = usePathname()
-  const { noti, setNoti, search, setSearch } = useHeaderModalState()
+  const { search, setSearch } = useHeaderModalState()
 
   useEffect(() => {
     const getCount = async () => {
@@ -32,7 +31,7 @@ export default function Header() {
 
   return (
     <header className="static my-[15px] flex justify-between leading-[50px]">
-      <div className="flex h-[50px] items-center">
+      <div className="flex justify-center items-center">
         <h1 className="pl-[20px]">
           <Link href="/">
             <Logo />
@@ -41,23 +40,22 @@ export default function Header() {
         <CategorySelection />
       </div>
       <div className="flex justify-center items-center mr-3">
-        <div className="relative w-full h-full flex justify-center items-center">
-          <IoMdNotificationsOutline
-            onClick={() => setNoti(true)}
-            className="text-sky-600 text-3xl mx-3"
-          />
+        <Link
+          href="/notification"
+          className="relative w-full h-full flex justify-center items-center"
+        >
+          <IoMdNotificationsOutline className="text-sky-600 text-4xl mx-3" />
           {num > 0 && (
-            <span className="flex justify-center items-center rounded-full absolute top-1 right-2 w-[18px] h-[18px] z-10 bg-red-500 text-center text-white text-xs">
+            <span className="flex justify-center items-center rounded-full absolute top-3 right-[10px] w-[18px] h-[18px] z-10 bg-red-500 text-center text-white text-xs">
               {num}
             </span>
           )}
-        </div>
+        </Link>
         <IoMdSearch
           onClick={() => setSearch(true)}
-          className="w-full h-full text-sky-600 text-3xl mx-3"
+          className="w-full h-full text-sky-600 text-4xl mx-3"
         />
       </div>
-      {noti && <NotificationModal />}
       {search && <SearchModal />}
     </header>
   )
