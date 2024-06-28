932fab75370b6905f24edfa08f6b6912e228b7da
yunii23 <munii1023@gmail.com>
2024-06-28 10:53:30 +0900

    알림 데이터 패칭, 삭제 기능 추가

--- src/containers/main/notification/NotificationContent.tsx
+++ src/containers/main/notification/NotificationContent.tsx
@@ -1,35 +1,36 @@
 import { useRouter } from 'next/navigation'
 import { PiTrashLight } from 'react-icons/pi'
 import { NotiListType } from '@/types/notiApiDataType'
-import { deleteNotification } from '@/utils/notificationApiActions'
 
-export default function NotificationContent({ item }: { item: NotiListType }) {
+export default function NotificationContent({
+  item,
+  deleteNoti,
+}: {
+  item: NotiListType
+  deleteNoti: (id: number) => void
+}) {
   const router = useRouter()
 
-  // eslint-disable-next-line consistent-return
-  const deleteNoti = async () => {
-    const data = await deleteNotification(item.id)
-
-    if (data.status !== 200) {
-      return null
-    }
-
-    router.refresh()
+  const handleButtonClick = () => {
+    router.push(item.link)
   }
 
   return (
-    <div className="bg-slate-100 my-[15px] px-[20px] py-[15px] rounded-3xl h-[80px]">
+    <div className="relative bg-slate-100 my-[20px] px-[20px] py-[15px] rounded-3xl">
+      <PiTrashLight
+        className="absolute w-5 h-5 right-4 bottom-4"
+        onClick={() => deleteNoti(item.id)}
+      />
       <button
         type="button"
-        className="w-full h-full"
-        // onClick={}
+        className="w-full h-full text-left"
+        onClick={handleButtonClick}
         aria-label="페이지 이동 버튼"
-      />
-      <p className="text-[17px] leading-[20px]">{item.content}</p>
-      <div className="flex justify-between">
-        <p className="text-[14px] leading-[40px]">{item.createdAt}</p>
-        <PiTrashLight className="w-5 h-5" onClick={deleteNoti} />
-      </div>
+      >
+        <p className="text-[17px] leading-[20px]">{item.title}</p>
+        <p className="text-[14px] leading-[20px] mt-[10px]">{item.content}</p>
+        <p className="text-[14px] leading-[10px] mt-[10px]">{item.createdAt}</p>
+      </button>
     </div>
   )
 }
