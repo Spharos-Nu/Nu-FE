8db6db9d8341f2fba20605f4620d1c77dcf30e8d
yunii23 <munii1023@gmail.com>
2024-06-28 11:06:52 +0900

    Refactor: 태그 # 정적으로 변경

--- src/containers/main/TagItem.tsx
+++ src/containers/main/TagItem.tsx
@@ -1,5 +1,5 @@
 import { TagType } from '@/types/mainType'
 
 export default function TagItem({ tag }: { tag: TagType }) {
-  return <p className="pr-[3px] text-[14px] text-[#a2a2a2]">{tag.name}</p>
+  return <p className="pr-[3px] text-[14px] text-[#a2a2a2]">#{tag.name}</p>
 }
