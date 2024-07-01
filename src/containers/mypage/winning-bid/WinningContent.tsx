4d5f1d28d14e98b0d6adf6f01391bcbc60186be6
SEOIHYEON <tjwn1408@naver.com>
2024-07-01 10:46:59 +0900

    Feat: 채팅 버그 수정

--- src/containers/mypage/winning-bid/WinningContent.tsx
+++ src/containers/mypage/winning-bid/WinningContent.tsx
@@ -18,8 +18,8 @@ export default function WinningContent({ goods }: { goods: BidType }) {
       <div className="rounded-b-xl border border-t-0 border-gray-300 flex justify-between relative">
         <div className="py-4 px-3 w-full">
           <p className="truncate w-full mb-3">{goods.name}</p>
-          <div className="bg-yellow-400 text-white flex justify-center items-end rounded-full px-[10px] py-3 text-sm">
-            <span className="text-gray-600">
+          <div className="text-[12px] bg-yellow-400 text-white flex justify-center items-end rounded-full px-[10px] py-3 text-sm">
+            <span className="text-[12px] text-gray-600">
               {goods.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
             </span>{' '}
             에 낙찰되었어요
